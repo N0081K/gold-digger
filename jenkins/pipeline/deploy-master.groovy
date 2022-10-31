751f85d8af02a536b8ade8e73a19be2404e1a477
harpener <harpener@gmail.com>
2022-10-31 13:04:28 +0100

    Increase rollout status timeout to 10m

--- jenkins/pipeline/deploy-master.groovy
+++ jenkins/pipeline/deploy-master.groovy
@@ -67,8 +67,8 @@ pipeline {
                         sed -i "s/\\$APP_VERSION/$APP_VERSION/g" kubernetes/gold-digger-api-deployment.yaml
                         sed -i "s/\\$APP_VERSION/$APP_VERSION/g" kubernetes/gold-digger-cron-deployment.yaml
                         kubectl --kubeconfig="$kube_config" apply -Rf kubernetes/
-                        kubectl --kubeconfig="$kube_config" rollout status deployment/gold-digger-deployment --timeout 2m
-                        kubectl --kubeconfig="$kube_config" rollout status deployment/gold-digger-cron-deployment --timeout 2m
+                        kubectl --kubeconfig="$kube_config" rollout status deployment/gold-digger-deployment --timeout 10m
+                        kubectl --kubeconfig="$kube_config" rollout status deployment/gold-digger-cron-deployment --timeout 10m
                         '''
                     }
                 }
